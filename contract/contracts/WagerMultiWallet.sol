3f89e42ef94058480c75408a35d0a94b9c60e3dd
Josh Delman <joshdelman@gmail.com>
2022-06-07 21:42:44 -0400

    start incorporating erc20, fix tests

--- contract/contracts/WagerMultiWallet.sol
+++ contract/contracts/WagerMultiWallet.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+
+  2022-06-06: plan to add support for ERC20
+    function transferFrom(
+      address from,
+      address to,
+      uint256 amount
+    ) external returns (bool);
+
+ */
+
 /** 
  * @title WagerMultiWallet
  * @dev First attempt to build a wager
  */
 contract WagerMultiWallet {
+  using SafeERC20 for IERC20;
+
   enum States { Invalid, Open, Closed, Resolved, Canceled }
 
   struct Wager {
@@ -14,7 +30,6 @@ contract WagerMultiWallet {
 
     // Don't need a hash because we'll be using an immutable IPFS URI
     // string metadataURI;
-
     uint32 vigBasisPoints;
     uint8 winningOutcome;
     mapping(address => mapping(uint8 => uint256)) betAmounts;
@@ -24,6 +39,9 @@ contract WagerMultiWallet {
 
     string proposition;
     bytes32[] outcomes;
+
+    bool isERC20;
+    IERC20 erc20Token;
   }
 
   uint256 private _nextId = 1;
@@ -40,7 +58,8 @@ contract WagerMultiWallet {
     uint32 _vigBasisPoints,
     // string memory _metadataURI,
     string memory _proposition,
-    bytes32[] memory _outcomes
+    bytes32[] memory _outcomes,
+    address _erc20Address
   ) public returns (uint256 wagerId) {
     uint256 id = _nextId++;
 
@@ -52,6 +71,14 @@ contract WagerMultiWallet {
     wager.state = States.Open;
     wager.proposition = _proposition;
     wager.outcomes = _outcomes;
+    
+    if (_erc20Address != address(0)) {
+      wager.isERC20 = true;
+      wager.erc20Token = IERC20(_erc20Address);
+    }
+    else {
+      wager.isERC20 = false;
+    }
 
     emit MintedWager(id, msg.sender);
 
@@ -64,6 +91,7 @@ contract WagerMultiWallet {
     require(wager.state == States.Open, 'Wager must be open');
     require(msg.sender != wager.owner, 'Wager owner cannot partipate');
     require(msg.value > 0, 'Bet must be greater than 0');
+    require(outcome >= 0 && outcome < wager.outcomes.length, 'Invalid outcome');
 
     wager.betAmounts[msg.sender][outcome] = msg.value;
     wager.totalPerOutcome[outcome] += msg.value;
@@ -72,6 +100,19 @@ contract WagerMultiWallet {
     require(wager.total < 2 ** 128, 'overflow');
 
     emit PlacedBet(index, msg.value, outcome);
+  }
+
+  function betERC20(uint256 index, uint8 outcome, uint256 amount) public {
+    Wager storage wager = _wagers[index];
+
+    require(wager.isERC20 == true, 'Must be an ERC20 style wager');
+    require(wager.state == States.Open, 'Wager must be open');
+    require(msg.sender != wager.owner, 'Wager owner cannot partipate');
+    require(amount > 0, 'Bet must be greater than 0');
+
+    _collectERC20(index, amount, msg.sender);
+
+    emit PlacedBet(index, amount, outcome);
   }
 
   function close(uint256 index) public {
@@ -92,6 +133,7 @@ contract WagerMultiWallet {
 
     require(wager.state == States.Closed, 'Wager must be closed to resolve');
     require(msg.sender == wager.owner, 'Only the owner can resolve the wager');
+    require(_winningOutcome < wager.outcomes.length, 'Invalid outcome');
 
     wager.winningOutcome = _winningOutcome;
 
@@ -99,6 +141,26 @@ contract WagerMultiWallet {
     uint256 ownerCut = getOwnerCut(index);
     wager.total -= ownerCut;
     payable(wager.owner).transfer(ownerCut);
+
+    wager.state = States.Resolved;
+
+    emit ResolvedWager(index, _winningOutcome);
+  }
+
+  // Does this really need to be a separate function? Would prefer to
+  // decrease the total "surface area"
+  function resolveERC20(uint256 index, uint8 _winningOutcome) public {
+    Wager storage wager = _wagers[index];
+
+    require(wager.state == States.Closed, 'Wager must be closed to resolve');
+    require(msg.sender == wager.owner, 'Only the owner can resolve the wager');
+
+    wager.winningOutcome = _winningOutcome;
+
+    // remove owner's cut from the total
+    uint256 ownerCut = getOwnerCut(index);
+    wager.total -= ownerCut;
+    _sendERC20(index, ownerCut, wager.owner);
 
     wager.state = States.Resolved;
 
@@ -113,6 +175,16 @@ contract WagerMultiWallet {
       / wager.totalPerOutcome[wager.winningOutcome];
     wager.betAmounts[msg.sender][wager.winningOutcome] = 0;
     payable(msg.sender).transfer(amount);
+  }
+
+  function claimERC20(uint256 index) public {
+    Wager storage wager = _wagers[index];
+
+    require(wager.state == States.Resolved, 'Wager must be resolved - the owner should call resolve()');
+    uint256 amount = wager.betAmounts[msg.sender][wager.winningOutcome] * wager.total
+      / wager.totalPerOutcome[wager.winningOutcome];
+    wager.betAmounts[msg.sender][wager.winningOutcome] = 0;
+    _sendERC20(index, amount, msg.sender);
   }
 
   function cancel(uint256 index) public {
@@ -135,6 +207,39 @@ contract WagerMultiWallet {
     uint256 amount = wager.betAmounts[msg.sender][outcome];
     wager.betAmounts[msg.sender][outcome] = 0;
     payable(msg.sender).transfer(amount);
+  }
+
+  function refundERC20(uint256 index, uint8 outcome) public {
+    Wager storage wager = _wagers[index];
+
+    require(wager.state == States.Canceled, 'Wager must be canceled for a refund');
+  
+    uint256 amount = wager.betAmounts[msg.sender][outcome];
+    wager.betAmounts[msg.sender][outcome] = 0;
+    
+    _sendERC20(index, amount, msg.sender);
+  }
+
+  /**
+
+    Internal functions to handle moving ERC20.
+
+   */
+
+  function _collectERC20(uint256 index, uint256 amount, address from) private {
+    // transfer amount from `from` address to this contract
+    Wager storage wager = _wagers[index];
+    require(wager.isERC20 == true, 'This wager does not support ERC20');
+
+    wager.erc20Token.transferFrom(from, address(this), amount);
+  }
+
+  function _sendERC20(uint256 index, uint256 amount, address to) private {
+    // transfer amount from this contract to `to` address
+    Wager storage wager = _wagers[index];
+    require(wager.isERC20 == true, 'This wager does not support ERC20');
+
+    wager.erc20Token.transferFrom(address(this), to, amount);
   }
 
   function getOwnerCut(uint256 index) public view returns (uint256) {
@@ -194,7 +299,9 @@ contract WagerMultiWallet {
     uint8 _state,
     uint256[] memory _totalsPerOutcome,
     string memory _proposition,
-    bytes32[] memory _outcomes
+    bytes32[] memory _outcomes,
+    bool _isERC20,
+    address _erc20Token
   ) {
     Wager storage wager = _wagers[index];
 
@@ -217,7 +324,9 @@ contract WagerMultiWallet {
       state,
       totalsPerOutcome,
       wager.proposition,
-      wager.outcomes
+      wager.outcomes,
+      wager.isERC20,
+      address(wager.erc20Token)
     );
   }
 }
--- contract/test/Wager.js
+++ contract/test/Wager.js
@@ -3,102 +3,127 @@ const BN = ethers.BigNumber;
 
 let minBetString = '10000000000000000';
 
+function getWagerObject(wagerArr) {
+  const args = [
+    'owner',
+    'endsAt',
+    'vigBasisPoints',
+    'winningOutcome',
+    'total',
+    'state',
+    'totalsPerOutcome',
+    'proposition',
+    'outcomes',
+    'isERC20',
+    'erc20Token',
+  ];
+
+  const out = {};
+
+  for (let i = 0; i < wagerArr.length; i++) {
+    out[args[i]] = wagerArr[i];
+  }
+
+  return out;
+}
+
 describe('Wager contract', function() {
   let Wager;
-  let hardhatWager;
   let owner;
   let addr1;
   let addr2;
   let addr3;
+  let wagerId;
 
   describe('Basic wager functionality', async function() {
     let minBet = BN.from(minBetString);
 
     before(async function() {
-      Wager = await ethers.getContractFactory("Wager");
+      WagerFactory = await ethers.getContractFactory("WagerMultiWallet");
       [owner, addr1, addr2, addr3] = await ethers.getSigners();
-      hardhatWager = await Wager.deploy(
-        'Russia-Ukraine war',
-        [
-          'Russia goes to war with Ukraine',
-          'Russia does not go to war with Ukraine',
-        ],
-        minBet,      // 0.01 ether
+
+      Wager = await WagerFactory.deploy();
+      
+      const wagerTx = await Wager.mintWager(
         60 * 60 * 24 * 30,      // 30 days in seconds
-        2,                      // max number of bettors
-        100                     // owner takes a 0.1% cut
+        100,                     // owner takes a 0.1% cut
+        'Will the Yankees win?',
+        [
+          ethers.utils.formatBytes32String('Yes'),
+          ethers.utils.formatBytes32String('No'),
+        ],
+        '0x0000000000000000000000000000000000000000',
       );
+      const wager = await wagerTx.wait();
+
+      const mintEvent = wager.events.find((event) => event.event === 'MintedWager');
+      const idHex = mintEvent.topics[1];
+      wagerId = idHex;
     });
 
     it('Should not allow a bet below the minimum', async function() {
-      await expect(hardhatWager.connect(addr1).bet(0, {
+      await expect(Wager.connect(addr1).bet(wagerId, 0, {
         value: 0
-      })).to.be.revertedWith('Bet must be at least the minimum');
+      })).to.be.revertedWith('Bet must be greater than 0');
     });
 
     it('Should not allow the owner to bet', async function() {
-      await expect(hardhatWager.bet(0, {
+      await expect(Wager.bet(wagerId, 0, {
         value: minBet
       })).to.be.revertedWith('Wager owner cannot partipate');
     });
 
     it('Should not allow betting on an invalid outcome', async function() {
-      await expect(hardhatWager.connect(addr1).bet(3, {
+      await expect(Wager.connect(addr1).bet(wagerId, 3, {
         value: minBet
       })).to.be.revertedWith('Invalid outcome');
     });
 
     it('Should allow normal bets', async function() {
-      await hardhatWager.connect(addr1).bet(0, {
+      await Wager.connect(addr1).bet(wagerId, 0, {
         value: minBet
       });
-      await hardhatWager.connect(addr2).bet(1, {
+      await Wager.connect(addr2).bet(wagerId, 1, {
         value: minBet
       });
-    });
-
-    it('Should not allow more than the max number of bettors', async function() {
-      await expect(hardhatWager.connect(addr3).bet(0, {
-        value: minBet
-      })).to.be.revertedWith('Too many bettors');
     });
 
     it('Should not allow non-owner to close', async function() {
-      await expect(hardhatWager.connect(addr1).close())
+      await expect(Wager.connect(addr1).close(wagerId))
         .to.be.revertedWith('Only the owner can close the wager');
     });
 
     it('Should not allow a non-closed wager to resolve', async function() {
-      await expect(hardhatWager.resolve(0))
+      await expect(Wager.resolve(wagerId, 1))
         .to.be.revertedWith('Wager must be closed to resolve');
     });
 
     it('Should allow owner to close', async function() {
-      await hardhatWager.close();
-      expect(await hardhatWager.getWagerState() === 1);
+      await Wager.close(wagerId);
+      expect(getWagerObject(await Wager.getWager(wagerId)).state === 1);
     });
 
     it('Should not allow a non-owner to resolve', async function() {
-      await expect(hardhatWager.connect(addr1).resolve(0))
+      await expect(Wager.connect(addr1).resolve(wagerId, 0))
         .to.be.revertedWith('Only the owner can resolve the wager');
     });
 
     it('Should not allow the owner to resolve to an invalid outcome', async function() {
-      await expect(hardhatWager.resolve(2))
+      await expect(Wager.resolve(wagerId, 2))
         .to.be.revertedWith('Invalid outcome');
     });
 
     it('Should transfer the owners cut upon resolution', async function() {
-      const totalBeforeResolve = await hardhatWager.total();
-      await hardhatWager.resolve(1); // trying to be optimistic :)
+      const totalBeforeResolve = getWagerObject(await Wager.getWager(wagerId)).total;
+      await Wager.resolve(wagerId, 1); // trying to be optimistic :)
       const expectedTotal = totalBeforeResolve
         .mul(BN.from('100'))
         .div(BN.from('100000'));
       await expect(expectedTotal === BN.from('19980000000000000'));
     });
-
-    it('Should just work', async function() {
-      
-    });
   });
+
+  // describe('ERC-20 wager', async function() {
+  //   it('Should ')
+  // });
 });
